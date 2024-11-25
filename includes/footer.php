a6c9d51f21077b9f6864c14995ce7b182ace6b32
RogerioSenac <roge.pupo.toledo@gmail.com>
2024-11-25 10:30:42 -0300

    geral

---
+++ includes/footer.php
@@ -0,0 +1,69 @@
+<!-- Rodapé Principal -->
+<section class="rodape-Acad">
+    <footer class="rodape">
+        <!-- Conteúdo do rodapé principal -->
+        <div class="card-group">
+            <div class="card-body-1">
+                <h3>Venha nos visitar.</h3>
+                <ul>
+                    <li><i class="fas fa-map-marker-alt"></i> Rua Januário Lisboa, 82, Vila Elias, Jacupiranga, SP</li>
+                    <li><i class="fas fa-envelope"></i> acquavidacademia@gmail.com</li>
+                </ul>
+            </div>
+        </div>
+        <div class="card-group">
+            <div class="card-body-3">
+                <h3>Horários da Academia</h3>
+                <p>
+                    <span class="servico-texto-realce">SEGUNDA À SEXTA</span><br>
+                    05:30 às 10:30<br>
+                    14:00 às 22:00<br>
+                    <span class="servico-texto-realce">SÁBADO</span><br>
+                    08:00 às 12:00<br>
+                    <span class="servico-texto-realce">DOMINGOS E FERIADOS</span><br>
+                    FECHADOS
+                </p>
+            </div>
+        </div>
+        <div class="card-group">
+            <div class="card-body-2">
+                <h3>Siga-nos nas redes sociais</h3>
+                <div class="social-icons">
+                    <a href="https://www.facebook.com/AcademiaAcquaVida/" class="facebook" target="_blank"><i class="fab fa-facebook fa-2x"></i></a>
+                    <a href="https://www.instagram.com/acquavidaacademia/" class="instagram" target="_blank"><i class="fab fa-instagram fa-2x"></i></a>
+                    <a href="https://api.whatsapp.com/send?phone=5513981555763&text=..." class="whatsapp" target="_blank"><i class="fab fa-whatsapp fa-2x"></i></a>
+                    <a href="mailto:acquavidacademia@gmail.com" class="email" target="_blank"><i class="fas fa-envelope"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</section>
+
+<!-- Rodapé DevRGT -->
+<section class="rodape-DevRGT">
+    <div class="interface" id="contato">
+        <div class="line-footer">
+            <!-- <div class="flex">
+                <div class="logo-footer">
+                    <img src="./Assets/imagem/logoDev.png" alt="Logotipo" class="img-fluid" style="max-height: 100px;">
+                </div>
+                <div class="btn-social">
+                    <a href="#"><button><i class="fa-brands fa-facebook"></i></button></a>
+                    <a href="https://www.instagram.com/developers.rgt" target="_blank"><button><i class="fa-brands fa-instagram"></i></button></a>
+                    <a href="https://www.linkedin.com/in/developers-rgt-862402309" target="_blank"><button><i class="fa-brands fa-linkedin"></i></button></a>
+                    <a href="https://www.youtube.com/@developers_rgt" target="_blank"><button><i class="fa-brands fa-youtube"></i></button></a>
+                    <a href="https://github.com/DevelopersRGT" target="_blank"><button><i class="fa-brands fa-github"></i></button></a>
+                </div>
+            </div> -->
+            <div class="line-footer borda">
+                <p class="designby">
+                    <span>Design By - Rogerio Pupo Toledo</span>
+                </p>
+                <p>
+                    <i class="fa-solid fa-envelope" id="faleicon"></i>
+                    <a id="falecom" href="mailto:falecomdevelopersrgt@gmail.com">falecomdevelopersrgt@gmail.com</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ includes/header.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Academia Acqua Vida</title>
+    <!-- Ícone Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- CSS Personalizado -->
+    <link rel="stylesheet" type="text/css" href="../Assets/css/estilo.css">
+</head>
+
+<body>
+    <!-- Cabeçalho e Navegação -->
+    <div class="container-fluid">
+        <a class="piscina" href="#">
+            <img src="/Projetos_Git/Projeto-Acqua-Vida/assets/imagem/logo_acqua_vida.jpg" alt="Logotipo"
+                class="img-fluid">
+        </a>
+    </div>
+    <h1 class="texto">Somos uma equipe promovendo Saúde!</h1>
+    <nav class="navbar navbar-expand-lg">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav" data-bs-theme="light">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="../index.php">Início</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./pages/quem_somos.php">Sobre Nós</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://www.facebook.com/AcademiaAcquaVida/photos_by?locale=pt_BR" target="_blank">Fotos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://www.facebook.com/AcademiaAcquaVida/videos?locale=pt_BR" target="_blank">Videos</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <!-- Scripts Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <script>
+        const navbar = document.querySelector('.navbar');
+        const navbarCollapse = document.querySelector('.navbar-collapse');
+
+        // Exibe o menu quando o mouse entra na área do menu
+        navbar.addEventListener('mouseenter', function() {
+            navbarCollapse.style.display = 'block';
+        });
+
+        // Esconde o menu quando o mouse sai da área do menu
+        navbar.addEventListener('mouseleave', function() {
+            navbarCollapse.style.display = 'none';
+        });
+    </script>
+
+</body>
+
+</html>
